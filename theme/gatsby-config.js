668de3796600a9b98c01cd78765a143162cc08bc
Arpit Sheth <shetharp@gmail.com>
2020-07-03 22:49:09 -0400

    Fix gatsby-source-filesystem config

--- theme/gatsby-config.js
+++ theme/gatsby-config.js
@@ -27,21 +27,21 @@ function GatsbyConfig(options) {
         resolve: "gatsby-source-filesystem",
         options: {
           name: "pages",
-          path: `${__dirname}/src/pages`,
+          path: `src/pages`,
         },
       },
       {
         resolve: "gatsby-source-filesystem",
         options: {
           name: "posts",
-          path: `${__dirname}/src/posts`,
+          path: `src/posts`,
         },
       },
       {
         resolve: "gatsby-source-filesystem",
         options: {
           name: "images",
-          path: `${__dirname}/src/images`,
+          path: `src/images`,
         },
       },
       "gatsby-plugin-sharp",
