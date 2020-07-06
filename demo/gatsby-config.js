a586f28b1cf06b829ff178d16f1528516ab8eeff
Arpit Sheth <shetharp@gmail.com>
2020-07-06 01:21:05 -0400

    Add gh-pages to demo site

--- demo/gatsby-config.js
+++ demo/gatsby-config.js
@@ -3,6 +3,7 @@ module.exports = {
     title: "Gatsby Theme Candor",
     description: `Gatsby theme candor demo site. Built by Arpit Sheth (@shetharp)`,
   },
+  pathPrefix: "/gatsby-theme-config",
   plugins: [
     {
       resolve: "@shetharp/gatsby-theme-candor",
